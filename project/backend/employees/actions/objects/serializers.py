f79f633c07534f66207acaaa127a2ed5d6445244
dhomini-rabelo <dhominisalgado@gmail.com>
2022-04-26 20:53:43 -0300

    employee serializer accept department as string

--- project/backend/employees/actions/objects/serializers.py
+++ project/backend/employees/actions/objects/serializers.py
@@ -1,12 +1,34 @@
 from rest_framework import serializers
-from backend.employees.app.models import Employee
+from backend.employees.app.models import Department, Employee
+from rest_framework.fields import empty
+from rest_framework.exceptions import ErrorDetail
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
 
-    def to_representation(self, instance):
+    def __init__(self, instance=None, data=empty, **kwargs):
+        if (data is not empty) and (isinstance(data.get('department'), str)):
+            """
+            This condition is used for create a new serializer with department field as string, also works with int type
+            """
+            department = Department.objects.filter(name=data['department'] or '').first()
+            data['department'] = department.id if department is not None else 0 # 0 is not possible id value
+        return super().__init__(instance, data, **kwargs)
+
+    @property
+    def errors(self):
+        if not hasattr(self, '_errors'):
+            msg = 'You must call `.is_valid()` before accessing `.errors`.'
+            raise AssertionError(msg)
+        
+        if self._errors.get('department') == [ErrorDetail(string='Invalid pk "0" - object does not exist.', code='does_not_exist')]:
+            self._errors = {**self._errors, 'department': ['Department not found']}
+        
+        return self._errors
+
+    def to_representation(self, instance: Employee):
         representation = super(EmployeeSerializer, self).to_representation(instance)
-        representation['department'] = instance.department.name
+        representation['department'] = instance.department.name # default value for foreign key is id
         return representation    
     
     class Meta:
